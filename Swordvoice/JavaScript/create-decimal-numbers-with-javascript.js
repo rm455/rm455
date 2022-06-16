9359301af04cdb97e50d5ce7429c8a7b431f7491
rm455 <58014332+rm455@users.noreply.github.com>
2022-06-15 23:29:18 -0400

    Delete Swordvoice/JavaScript directory

--- Swordvoice/JavaScript/create-decimal-numbers-with-javascript.js
+++
@@ -1,8 +0,0 @@
-const myDecimal = 5.7;
-
-// Only change code below this line
-/*
-We can store decimal numbers in variables too. Decimal numbers are sometimes referred to as floating point numbers or floats.
-
-Note: computers represent numbers with finite precision. That's why floating-point operations cannot precisely represent true arithmetic operations, and this leads to many surprising situations. if you are facing one of these issues, open topic in the freeCodeCamp forum.
-*/
--- Swordvoice/JavaScript/finding-a-remainder-in-javascript.js
+++
@@ -1,5 +0,0 @@
-//const remainder = 2 ;
-/*Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.*/
-
-var remainder = 11 % 3;
-
