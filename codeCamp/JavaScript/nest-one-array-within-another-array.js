fde1faafd1aae273a9b85d7acc79544265ad530b
rm455 <58014332+rm455@users.noreply.github.com>
2022-07-04 22:36:47 -0400

    Add files via upload

---
+++ codeCamp/JavaScript/nest-one-array-within-another-array.js
@@ -0,0 +1,16 @@
+/*
+Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+
+Create a nested array called myArray.
+
+// Only change code below this line
+const myArray = [];
+
+*/
+
+// Only change code below this line
+const myArray = [["My nestedArray", 1], 1];
