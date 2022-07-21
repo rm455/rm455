cf806eabd0d318fb0a9b37bf2f0e4356317ca999
rm455 <58014332+rm455@users.noreply.github.com>
2022-07-20 21:36:58 -0400

    Add files via upload

---
+++ codeCamp/JavaScript/shopping-list.js
@@ -0,0 +1,14 @@
+/*
+Shopping List
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+
+const myList = [];
+
+*/
+
+const myList = [["Batman", 1], ["Goku", 2], ["Zub Zero", 3], ["Mario", 4], ["Ren & Stimpy", 5]];
